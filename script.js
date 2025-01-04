ac5092ccbee839250526fce09c6bc03d775e0017
Ternamdoo <jerryaliyese@gmail.com>
2025-01-04 11:06:46 +0100

    Add functionality for user to enter their choice

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Hello World")
 // Function to generate a random integer between 0 and any given number
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+  };
 
 // Generate a random integer between 0,1, and 2 and return a choice depending on the returned valuw
 function getComputerChoice () {
@@ -18,4 +18,11 @@ function getComputerChoice () {
     } else {
         alert("The code logic is broken!")
     }
-}
+};
+
+// Get choice from human user
+function getHumanChoice () {
+    let choice = prompt("Enter either: 'rock', 'paper', or 'scissors' ");
+    return choice;
+};
+console.log(getHumanChoice());
