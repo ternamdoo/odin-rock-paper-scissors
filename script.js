e9231a6e3a61f6afa0706c5d5df8b0cee9a93916
Ternamdoo <jerryaliyese@gmail.com>
2025-01-04 10:20:59 +0100

    Add the logic to get computer choice
    The logic uses two functions:
    
    1. getRandomInt() generates a random integer between 0 and any given
    number.
    
    2. getComputerChoice() with a hard-coded value of 3. This generates
    either 0, 1, or 2. These values are then used to set the chice for the
    computer (to rock, paper, or scissors) using conditional logic.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello World")
+
+// Function to generate a random integer between 0 and any given number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+// Generate a random integer between 0,1, and 2 and return a choice depending on the returned valuw
+function getComputerChoice () {
+    let randomNumber = getRandomInt(3);
+    console.log(randomNumber);
+    if (randomNumber === 0) {
+        return "rock"
+    } else if (randomNumber === 1) {
+        return "paper"
+    } else if (randomNumber === 2) {
+        return "scissors"
+    } else {
+        alert("The code logic is broken!")
+    }
+}
