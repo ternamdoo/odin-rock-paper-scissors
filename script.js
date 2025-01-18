8e909d1b37b455ab2f8bd79f103bee1730be97e9
Ternamdoo <jerryaliyese@gmail.com>
2025-01-18 03:47:52 +0100

    Add function to get human user's choice

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3)
     switch (randomInt) {
         case 0:
-            return "rock"
+            return "rock";
         case 1:
-            return "paper"
+            return "paper";
         case 2:
-            return "scissors"
+            return "scissors";
+    }
+}
+
+// Get human choice
+function getHumanChoice() {
+    let humanInput = prompt("Enter a valid choice between 'rock', 'paper, or 'scissors': ")
+    let humanInputLowercase = humanInput.toLowerCase()
+    switch (humanInputLowercase) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return humanInputLowercase
+        default:
+            console.log("You did not enter a valid game keyword. The next round wont be played successfully")
+            return ""
     }
 }
