b7e7dec4b545c898c11cc3374d5dcb01ad3773fe
Ternamdoo <jerryaliyese@gmail.com>
2025-01-18 03:27:17 +0100

    Add function to randomly generate computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+// randomly generate computer choice
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3)
+    switch (randomInt) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+    }
+}
