02fe0d41cf68461fd120db8ca13325590f0986f1
Ternamdoo <jerryaliyese@gmail.com>
2025-01-21 02:18:36 +0100

    Add playGame() function for full game logic

--- script.js
+++ script.js
@@ -33,41 +33,16 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-// Play one round
-function playRound(humanChoice, computerChoice) {
-    console.log("Rock, Paper, Scissors, shoot!");
-    console.log("Human:", humanChoice);
-    console.log("Computer:", computerChoice);
-    switch (true) {
-        // if the round's a tie, do nothing
-        case (humanChoice === computerChoice):
-            console.log(`It's a tie! Both players chose ${humanChoice}`);
-            break;
-        // increment human player score if human wins
-        case (humanChoice === "rock" && computerChoice === "scissors"):
-        case (humanChoice === "paper" && computerChoice === "rock"):
-        case (humanChoice === "scissors" && computerChoice === "paper"):
-            console.log(`Bravo! ${humanChoice} beats ${computerChoice}. Human wins`);
-            humanScore++;  
-            break;
-        // increment computer score if computer wins
-        case (computerChoice === "rock" && humanChoice === "scissors"):
-        case (computerChoice === "paper" && humanChoice === "rock"):
-        case (computerChoice === "scissors" && humanChoice === "paper"):
-            console.log(`Bravo! ${computerChoice} beats ${humanChoice}. Computer wins`);
-            computerScore++;  
-            break;
-        // if the code block below runs, it means there's a flaw in the game logic above. DEBUG!!!
-        default:
-            console.log("Something's wrong. This live isn't supposed to execute.")
-    }
 
-    console.log("Human score:", humanScore);
-    console.log("Computer score:", computerScore);
-}
 
 // store players choices for a single game round
 const humanSelection = getComputerChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+
+// play 5 rounds and announce the winner if there's any
+function playGame() {
+    
+}
