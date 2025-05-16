d5f86a4fcb27a9b3fe9cd03cbaf684229a424187
Ternamdoo <jerryaliyese@gmail.com>
2025-05-16 22:40:21 +0100

    Clean up the script some more

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ humanOptions.forEach((option) => {
     })
 });
 
+
 // randomly pick computer choice from an array of options
 function getComputerChoice() {
     const validGameChoices = ["rock", "paper", "scissors"];
@@ -40,18 +41,15 @@ function playRound(humanSelection, computerSelection) {
             announceRoundWinner.textContent = `Bravo! ${humanSelection} beats ${computerSelection}. Human wins`;
             humanScore.textContent = Number(humanScore.textContent) + 1;
             break;
-        // increment computer score if computer wins
-        case (computerSelection === "rock" && humanSelection === "scissors"):
-        case (computerSelection === "paper" && humanSelection === "rock"):
-        case (computerSelection === "scissors" && humanSelection === "paper"):
-            announceRoundWinner.textContent = `Bravo! ${computerSelection} beats ${humanSelection}. Computer wins`;
-            computerScore.textContent = Number(computerScore.textContent) + 1;
-            break;
-        // if the code block below runs, it means there's a flaw in the game logic above. DEBUG!!!
-        default:
-            announceRoundWinner.textContent = "Something's wrong. This line isn't supposed to execute.";
-    }
-
-    console.log("Human score:", humanScore);
-    console.log("Computer score:", computerScore);
+            // increment computer score if computer wins
+            case (computerSelection === "rock" && humanSelection === "scissors"):
+                case (computerSelection === "paper" && humanSelection === "rock"):
+                    case (computerSelection === "scissors" && humanSelection === "paper"):
+                        announceRoundWinner.textContent = `Bravo! ${computerSelection} beats ${humanSelection}. Computer wins`;
+                        computerScore.textContent = Number(computerScore.textContent) + 1;
+                        break;
+                        // if the code block below runs, it means there's a flaw in the game logic above. DEBUG!!!
+                        default:
+                            announceRoundWinner.textContent = "Something's wrong. This line isn't supposed to execute.";
+                        }
 }
